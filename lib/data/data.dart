a505315ac057033b3af249b3305f372e8bf4e398
praguru14 <48213609+praguru14@users.noreply.github.com>
2021-06-10 19:33:43 +0530

    Webview

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -12,11 +12,11 @@ List<catModel> getCat() {
   category.add(catmodel);
 
   //2
-  catmodel = new catModel();
-  catmodel.catName = "Entertainment";
-  catmodel.imgUrl =
-      "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
-  category.add(catmodel);
+  // catmodel = new catModel();
+  // catmodel.catName = "Entertainment";
+  // catmodel.imgUrl =
+  //     "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  // category.add(catmodel);
 
   //3
   catmodel = new catModel();
@@ -25,30 +25,30 @@ List<catModel> getCat() {
       "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
   category.add(catmodel);
 
-  //4
-  catmodel = new catModel();
-  catmodel.catName = "Health";
-  catmodel.imgUrl =
-      "https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1595&q=80";
-  category.add(catmodel);
+  // //4
+  // catmodel = new catModel();
+  // catmodel.catName = "Health";
+  // catmodel.imgUrl =
+  //     "https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1595&q=80";
+  // category.add(catmodel);
+  //
+  // //5
+  // catmodel = new catModel();
+  // catmodel.catName = "Science";
+  // catmodel.imgUrl =
+  //     "https://images.unsplash.com/photo-1554475901-4538ddfbccc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80";
+  // category.add(catmodel);
+
+  // //5
+  // catmodel = new catModel();
+  // catmodel.catName = "Sports";
+  // catmodel.imgUrl =
+  //     "https://images.unsplash.com/photo-1495563923587-bdc4282494d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80";
+  // category.add(catmodel);
 
   //5
   catmodel = new catModel();
-  catmodel.catName = "Science";
-  catmodel.imgUrl =
-      "https://images.unsplash.com/photo-1554475901-4538ddfbccc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80";
-  category.add(catmodel);
-
-  //5
-  catmodel = new catModel();
-  catmodel.catName = "Sports";
-  catmodel.imgUrl =
-      "https://images.unsplash.com/photo-1495563923587-bdc4282494d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80";
-  category.add(catmodel);
-
-  //5
-  catmodel = new catModel();
-  catmodel.catName = "Technology";
+  catmodel.catName = "Top";
   catmodel.imgUrl =
       "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
   category.add(catmodel);
